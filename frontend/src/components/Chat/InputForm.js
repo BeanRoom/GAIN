e1597006779f899a348eff02ee452d37f74f5084
Dotsung <andy395023@gmail.com>
2019-04-23 23:49:49 +0900

    Chat input 수정

--- frontend/src/components/Chat/InputForm.js
+++ frontend/src/components/Chat/InputForm.js
@@ -1,17 +1,24 @@
 // @flow
 import React from 'react';
 import styled from 'styled-components';
+import oc from 'open-color';
 
 const Form = styled.form`
     display: flex;
+    margin: 10px;
 `
 
 const StyledInput = styled.input`
     background-color: #ffffff;
     border-top: 1px solid #000000;
+    border-radius: 5px;
     width: 100%;
     height: 20px;
     font-size: 1em;
+
+    &:focus{
+        outline: none;
+    }
 `
 
 class InputForm extends React.Component {
@@ -40,7 +47,6 @@ class InputForm extends React.Component {
                     value={this.state.msg}
                     onChange={this.handleChange}
                 />
-                <input type="submit" value="Submit" />
             </Form>
         )
     }
