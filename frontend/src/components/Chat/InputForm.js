640969cce4da22d5ebd6bf2aed60e852b5c08677
Dotsung <andy395023@gmail.com>
2019-04-24 09:35:45 +0900

    textarea

--- frontend/src/components/Chat/InputForm.js
+++ frontend/src/components/Chat/InputForm.js
@@ -9,17 +9,22 @@ const Form = styled.form`
 `
 
 const StyledInput = styled.input`
+`
+
+const TextArea = styled.textarea`
     background-color: #ffffff;
     border-top: 1px solid #000000;
     border-radius: 5px;
     width: 100%;
     height: 20px;
     font-size: 1em;
+    overflow-y: hidden;
 
     &:focus{
         outline: none;
     }
 `
+
 
 class InputForm extends React.Component {
     state={
@@ -32,20 +37,34 @@ class InputForm extends React.Component {
         })
     }
 
-    handleSubmit = (event) => {
-        event.preventDefault();
+    onSubmit = () => {
         this.props.sendMessage(this.state.msg);
         this.setState({
             msg: ''
         })
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.onSubmit();
+    }
+
+    onKeyDown = (e) => {
+        if(e.keyCode === 13){
+            this.onSubmit();
+        }
+    }
+
     render() {
         return (
             <Form onSubmit={this.handleSubmit}>
                 <StyledInput
+                    type="file"
+                />
+                <TextArea
                     value={this.state.msg}
                     onChange={this.handleChange}
+                    onKeyDown={this.onKeyDown}
                 />
             </Form>
         )
