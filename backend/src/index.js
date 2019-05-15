cdd68ccb0c261726e4ee7619441dd9d95e3e763e
Hwarang Choi <az8606@naver.com>
2019-05-15 21:05:12 +0900

    use Sequelize.js instead of mysql module

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,34 +2,28 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { connection } from 'lib/db.js';
 
 import Koa from 'koa';
+//const Koa = require('koa');
 import Router from 'koa-router';
 import cors  from '@koa/cors';
+
 
 const app = new Koa();
 const router = new Router();
 import api from './api';
 
-import mariadb from 'mysql';
-//import mongoose from 'mongoose';
+// MariaDB 연결을 위해 sequelize를 사용합니다.
+import { sequelize } from './models';
+sequelize.sync();
+
+
 import bodyParser from 'koa-bodyparser';
 
 //import { jwtMiddleware } from 'lib/token';
 
 app.use(cors());
 
-connection.connect();
-// mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
-// // mongodb 연결
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(
-//     (response) => {
-//         console.log('Successfully connected to mongodb');
-//     }
-// ).catch(e => {
-//     console.error(e);
-// });
 
 const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
 
