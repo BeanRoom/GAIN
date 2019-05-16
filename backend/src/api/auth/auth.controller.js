0436261cc3e280dd38c03b952913404b4b410957
Hwarang Choi <az8606@naver.com>
2019-05-16 12:16:43 +0900

    Joi 형식 검사, 아이디 중복 검사, account db 저장

--- backend/src/api/auth/auth.controller.js
+++ backend/src/api/auth/auth.controller.js
@@ -1,10 +1,14 @@
 import Joi from 'joi';
-import mysql from 'mysql';
+import crypto from 'crypto';
+import { account, student, teacher, guest } from '../../models';
+
+//import { Maria, sequelize } from 'lib/db.js';
 
 // env 환경 변수 불러오기
 import dotenv from 'dotenv';
 dotenv.config();
 
+// post로 받은 authCode의 값에 따라서 권한을 설정해주기 위한 함수
 const authorizeUser = (authCode) => {
     
     if(authCode == process.env.studentCode)
@@ -16,17 +20,62 @@ const authorizeUser = (authCode) => {
     else return 3;
 }
 
-
 export const Register = async (ctx) => {
     
-    const FirstRegisteration = Joi.object().key({
+    const FirstRegisteration = Joi.object().keys({
         id : Joi.string().alphanum().min(6).max(50).required(),
         password : Joi.string().min(6).max(50).required(),
-        authCode : Joi.alphanum().required()  
+        authCode : Joi.string().alphanum().required()  
     });
 
-    
+    const firstResult = Joi.validate(ctx.request.body, FirstRegisteration);
 
+    // joi의 형식 검사에서 에러 발생 시 400 에러코드를 전송하고, body에 001 이라는 내용을 담아 joi 오류임을 알려줌
+    if(firstResult.error) {
+        ctx.status = 400;
+        ctx.body = {
+            "error" : "001"
+        }
+        return;
+    }
+
+    const exist = await account.findAll({
+        where: {
+            id : ctx.request.body.id
+        }
+    });
+
+    if(exist.length){
+        console.log("this column is already existed");
+
+        ctx.status = 400;
+        ctx.body = {
+            "error" : "002"
+        }
+        return;
+    }
+
+    let id = ctx.request.body.id;
+    let password = ctx.request.body.password;
+    let password2 = crypto.createHmac('sha256', process.env.Password_KEY).update(password).digest('hex');
+    const users = await account.findAll();
+    let userCode = (users.length+1).toString();
+
+    for(var i=0;userCode.length<4;i++){
+        userCode = "0" + userCode;
+    }
+
+    userCode = "U" + userCode;
+
+    let auth = authorizeUser(ctx.request.body.authCode);
+
+    console.log(`ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\nid 값 : ${id}\npassword 값 : ${password}\nhash 값 : ${password2}\nuserCode 값 : ${userCode}\nauth 값 : ${auth}\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ`);
     
-    ctx.body = 
+    account.create({
+        id : id,
+        password : password2,
+        userCode : userCode,
+        auth : auth
+    });
 }
+
--- backend/src/api/auth/index.js
+++ backend/src/api/auth/index.js
@@ -1,9 +1,9 @@
 import Router from 'koa-router';
-import * as authCtrl from './auth.controller';
+import { Register } from './auth.controller';
 
 const auth = new Router();
 
-// auth.post('/register/local', authCtrl.localRegister);
+auth.post('/register', Register);
 // auth.post('/login/local', authCtrl.localLogin);
 // auth.get('/exists/:key(email|username)/:value', authCtrl.exists);
 // auth.post('/logout', authCtrl.logout);
