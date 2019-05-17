463cbabbad17d30e1cd5e8a9ed88ae17970f77ac
Hwarang Choi <az8606@naver.com>
2019-05-17 23:39:47 +0900

    add CheckDetail

--- backend/src/api/auth/index.js
+++ backend/src/api/auth/index.js
@@ -1,9 +1,10 @@
 import Router from 'koa-router';
-import { Register } from './auth.controller';
+import { Register, CheckDetail } from './auth.controller';
 
 const auth = new Router();
 
 auth.post('/register', Register);
+//auth.get('/check', CheckDetail);
 // auth.post('/login/local', authCtrl.localLogin);
 // auth.get('/exists/:key(email|username)/:value', authCtrl.exists);
 // auth.post('/logout', authCtrl.logout);
